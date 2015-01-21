bf8a228690a944465daf1e319421c15956fe3dba
Javier Segovia Córdoba <thedahnark@gmail.com>
2015-01-21 22:35:32 +0100

    Some themes

--- app/src/main/java/com/example/javier/NavigationDrawerAllVersions/MainActivity.java
+++ app/src/main/java/com/example/javier/NavigationDrawerAllVersions/MainActivity.java
@@ -1,24 +1,55 @@
 package com.example.javier.NavigationDrawerAllVersions;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.FrameLayout;
 
 
 public class MainActivity extends ActionBarActivity {
     Toolbar toolbar;
     DrawerLayout mDrawerLayout;
+    SharedPreferences sharedPreferences;
+    CardView cardView1, cardView2, cardView3, cardView4, cardView5, cardView6;
+    int theme;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Select theme saved by user
+        sharedPreferences = getSharedPreferences("THEMES", Context.MODE_PRIVATE);
+        theme = sharedPreferences.getInt("THEME", 0);
+        switch (theme){
+            case 1: setTheme(R.style.AppTheme);
+                break;
+            case 2: setTheme(R.style.AppTheme2);
+                break;
+            case 3: setTheme(R.style.AppTheme3);
+                break;
+            case 4: setTheme(R.style.AppTheme4);
+                break;
+            case 5: setTheme(R.style.AppTheme5);
+                break;
+            case 6: setTheme(R.style.AppTheme6);
+                break;
+            default: setTheme(R.style.AppTheme);
+                break;
+        }
+
+        // Set content to the view
         setContentView(R.layout.activity_main);
 
         //Setup Toolbar
@@ -37,6 +68,63 @@ public class MainActivity extends ActionBarActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
         mDrawerToggle.syncState();
+
+        // Save one theme pressing a button (check style to see five themes)
+        final SharedPreferences.Editor editor = sharedPreferences.edit();
+        cardView1 = (CardView) findViewById(R.id.card_view);
+        cardView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editor.putInt("THEME", 1).apply();
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        cardView2 = (CardView) findViewById(R.id.card_view2);
+        cardView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editor.putInt("THEME", 2).apply();
+                Intent intent = new Intent(MainActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        cardView3= (CardView) findViewById(R.id.card_view3);
+        cardView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editor.putInt("THEME", 3).apply();
+                Intent intent = new Intent(MainActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        cardView4 = (CardView) findViewById(R.id.card_view4);
+        cardView4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editor.putInt("THEME", 4).apply();
+                Intent intent = new Intent(MainActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        cardView5 = (CardView) findViewById(R.id.card_view5);
+        cardView5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editor.putInt("THEME", 5).apply();
+                Intent intent = new Intent(MainActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        cardView6 = (CardView) findViewById(R.id.card_view6);
+        cardView6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editor.putInt("THEME", 6).apply();
+                Intent intent = new Intent(MainActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -61,7 +149,7 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void setNavigationStatusBar(){
+    public void setNavigationStatusBar() {
 
         // Fix portrait issues
         if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
@@ -86,14 +174,14 @@ public class MainActivity extends ActionBarActivity {
 
         // Fix landscape issues (only Lollipop)
         if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            if (Build.VERSION.SDK_INT >= 19){
+            if (Build.VERSION.SDK_INT >= 19) {
                 TypedValue typedValue19 = new TypedValue();
                 MainActivity.this.getTheme().resolveAttribute(R.attr.colorPrimary, typedValue19, true);
                 final int color = typedValue19.data;
                 FrameLayout statusBar = (FrameLayout) findViewById(R.id.statusBar);
                 statusBar.setBackgroundColor(color);
             }
-            if (Build.VERSION.SDK_INT >= 21){
+            if (Build.VERSION.SDK_INT >= 21) {
                 TypedValue typedValue = new TypedValue();
                 MainActivity.this.getTheme().resolveAttribute(R.attr.colorPrimaryDark, typedValue, true);
                 final int color = typedValue.data;
