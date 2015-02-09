5fd4e95c2781e697ab77a1b5edba808fc17113fe
Javier Segovia Córdoba <thedahnark@gmail.com>
2015-02-09 20:21:39 +0100

    Added Material Design tabs inside design item (navigation drawer).

---
+++ app/src/main/java/com/example/javier/NavigationDrawerAllVersions/JSONParser.java
@@ -0,0 +1,7 @@
+package com.example.javier.NavigationDrawerAllVersions;
+
+/**
+ * Created by Javier on 23/01/2015.
+ */
+public class JSONParser {
+}
---
+++ app/src/main/java/com/example/javier/NavigationDrawerAllVersions/onTaskRemoved.java
@@ -0,0 +1,7 @@
+package com.example.javier.NavigationDrawerAllVersions;
+
+/**
+ * Created by Javier on 25/01/2015.
+ */
+public class onTaskRemoved {
+}
