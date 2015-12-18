6f7236fcb404f1b70c2333078785151b412684fc
irosle <bboyoswaldo@gmail.com>
2015-12-18 12:36:25 -0600

    doesn't work
    doesn't work with the implementation of desing:AppLayout

--- app/src/main/java/com/example/javier/MaterialDesignApp/Utils/ScrimInsetsFrameLayout.java
+++ app/src/main/java/com/example/javier/MaterialDesignApp/Utils/ScrimInsetsFrameLayout.java
@@ -53,13 +53,12 @@ public class ScrimInsetsFrameLayout extends FrameLayout {
     }
 
     private void init(Context context, AttributeSet attrs, int defStyle) {
-        final TypedArray a = context.obtainStyledAttributes(attrs,
-                R.styleable.ScrimInsetsView, defStyle, 0);
+        /*final TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.ScrimInsetsView, defStyle, 0);
         if (a == null) {
             return;
         }
         mInsetForeground = a.getDrawable(R.styleable.ScrimInsetsView_insetForeground);
-        a.recycle();
+        a.recycle();*/
 
         setWillNotDraw(true);
     }
